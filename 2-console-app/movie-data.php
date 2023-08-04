f04560216f44c7c65f33274cea6a60b124495d2c
hehnull <efanken17@gmail.com>
2023-08-04 18:58:00 +0200

    it's already done

--- 2-console-app/movie-data.php
+++ 2-console-app/movie-data.php
@@ -1,5 +1,6 @@
 <?php
 declare(strict_types=1);
+require "app-functions.php";
 
 $movies = [
     [
@@ -159,44 +160,4 @@ while (true)
         displayMovies($filteredMovies);
         break;
     }
-}
-
-function verifyAge($typedAge): bool
-{
-    if (is_numeric($typedAge) && (int)$typedAge != 0)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-function displayMovies($filteredMovies): void
-{
-    $numberMovie = 0;
-    foreach ($filteredMovies as $movie)
-    {
-        $numberMovie = $numberMovie + 1;
-        displayMovie($movie, $numberMovie);
-    }
-}
-
-function displayMovie($movie, $moviePosition): void
-{
-    echo "${moviePosition}. {$movie["title"]} ({$movie["release_year"]}), {$movie["age_restriction"]}+. Rating - {$movie["rating"]}\n";
-}
-
-function filterMoviesByAgeRestriction($initialMoviesList, $userAge): array
-{
-    $sortedMovies = [];
-    foreach ($initialMoviesList as $movie)
-    {
-        if ($movie["age_restriction"] <= $userAge)
-        {
-            $sortedMovies[] = $movie;
-        }
-    }
-    return $sortedMovies;
 }
